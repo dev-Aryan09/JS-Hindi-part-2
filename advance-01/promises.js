2712126e91e1d225f144354c1d95b22648e2cb97
AryanSaini <09aryan.lucky@gmail.com>
2024-10-14 23:06:42 +0530

    add fetch

--- advance-01/promises.js
+++ advance-01/promises.js
@@ -53,26 +53,48 @@
 //   })
 //   .finally(() => console.log("The promise is either resolvedf or rejected"));
 
-
 //-------------------  async and await ----------------------------
 
-const promiseFive = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = false;
-    if (!error) {
-      resolve({ userName: "JavaScript", password: 123 });
-    } else {
-      reject("ERROR: JS went wrong");
-    }
-  }, 1000);
-});
+// const promiseFive = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = false;
+//     if (!error) {
+//       resolve({ userName: "JavaScript", password: 123 });
+//     } else {
+//       reject("ERROR: JS went wrong");
+//     }
+//   }, 1000);
+// });
 
-async function consumePromiseFive() {
+// async function consumePromiseFive() {
+//   try {
+//     const response = await promiseFive;
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// consumePromiseFive();
+
+async function getAllUsers() {
   try {
-    const response = await promiseFive;
-    console.log(response);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.log(error);
+    console.log("E:", error);
   }
 }
-consumePromiseFive();
+getAllUsers()
+
+
+fetch("https://api.github.com/users/hiteshchoudhary")      // fetch() gets in priority 
+  .then((response) => {
+    // console.log(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
