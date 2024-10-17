ea81e4410590f5b017f0b1d80908da10e8f148df
AryanSaini <09aryan.lucky@gmail.com>
2024-10-17 21:46:29 +0530

    add about constructor function

--- classes_and_oop/oop.js
+++ classes_and_oop/oop.js
@@ -1,4 +1,4 @@
-// Object literal
+// ---------------------------------------- Object literal----------------------------------
 // Object literal is literally an object in JS
 
 const user = {
@@ -10,7 +10,6 @@ const user = {
     // console.log("Got user details from database");
     // console.log(`Username: ${this.username}`);
     console.log(this);
-    
   },
 };
 
@@ -18,3 +17,28 @@ const user = {
 // console.log(user.getUserInfo());
 
 // console.log(this);   // gives empty paranthesis {}
+
+//------------------------ Constructor function ---------------------------------------------
+
+// -> Sometimes we need to create many objects of the same type.
+// -> To create an object type we use an object constructor function.
+
+// NOTE -> In the constructor function, this has no value. The value of this will become the new object when a new object is created.
+
+function User(username, logincount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = logincount;
+  this.isLoggedIn = isLoggedIn;
+
+  this.greeting = function () {
+    console.log(`Welcome ${this.username}`);
+  };
+
+  return this;
+}
+
+const userOne = new User("hitesh", 12, true);
+const userTwo = new User("ChaiAurCOde", 11, false);
+console.log(userOne);
+console.log(userOne.constructor);   // Tells about the reference
+// console.log(userTwo);
